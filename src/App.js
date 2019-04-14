9416943eb0b7e0b94008c9859f3a178263254b9b
noy hadaria <noyhadaria@gmail.com>
2019-04-14 19:15:58 +0300

    App to function component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import NavBar from './navbar/NavBar';
 import Search from './search/Search'
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <NavBar />
-        <Search />
-      </div>
-    );
-  }
+const App = () => {
+  return (
+    <div>
+      <NavBar />
+      <Search />
+    </div>
+  );
 }
 
 export default App;
